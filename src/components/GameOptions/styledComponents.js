ee5eb65d81b40329697b9d21f250c7f16fce582c
PavanKuchana <Pavankuchana007@gmail.com>
2021-10-28 13:26:12 +0000

    Rock Paper Scissors React JS

---
+++ src/components/GameOptions/styledComponents.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const OptionImage = styled.img`
+  width: 140px;
+  height: 140px;
+  @media screen and (min-width: 768px) {
+      width: 170px;
+      height: 170px;
+    }
+  }
+`
+export const OptionListItem = styled.li`
+  list-style-type: none;
+  display: flex;
+`
+export const GameOptionButton = styled.button`
+  outline: none;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+`
